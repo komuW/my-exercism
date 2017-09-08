fa47edb905906929f2366c6dda6db9b7230ac38b
komu <komuw05@gmail.com>
2017-09-08 18:19:21 +0300

    start impl

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -1,8 +1,18 @@
-package gigasecond
+package main
 
 // import path for the time package from the standard library
 import "time"
+import "fmt"
 
 const testVersion = 4
 
-func AddGigasecond(time.Time) time.Time
+//func AddGigasecond(time.Time) time.Time
+
+func main() {
+	time.Parse(layout string, value string)
+	x := time.Now()
+	fmt.Println("x", x)
+	y := x.Unix()
+	fmt.Println("y", y)
+
+}
