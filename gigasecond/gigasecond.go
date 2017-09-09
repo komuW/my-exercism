899032bb45076b307cb74796e6a1aeb79bcf52c9
komu <komuw05@gmail.com>
2017-09-09 13:19:24 +0300

    add proper test

--- gigasecond/gigasecond.go
+++ gigasecond/gigasecond.go
@@ -1,18 +1,11 @@
-package main
+package gigasecond
 
-// import path for the time package from the standard library
 import "time"
-import "fmt"
 
 const testVersion = 4
 
-//func AddGigasecond(time.Time) time.Time
-
-func main() {
-	time.Parse(layout string, value string)
-	x := time.Now()
-	fmt.Println("x", x)
-	y := x.Unix()
-	fmt.Println("y", y)
-
+// AddGigasecond adds a gigasec to the given time
+func AddGigasecond(inputTime time.Time) time.Time {
+	age := inputTime.Add(time.Second * 1000000000)
+	return age
 }
