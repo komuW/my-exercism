4786b5aae1ccd8e9e4c8327389085926973a9a7f
komu <komuw05@gmail.com>
2017-09-11 00:44:45 +0300

    complete

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,34 +1,35 @@
-package main
+package hamming
 
 import (
-	"fmt"
+	"errors"
 )
 
 const testVersion = 6
 
+// Distance calculates the hamming distance between two strings
 func Distance(x, y string) (int, error) {
 	a := []byte(x)
 	b := []byte(y)
+
 	lenOfstring := len(a)
 	lenOfb := len(b)
-	missMatches := make([]int,0)
+	missMatches := make([]int, 0)
 
-	for i:=0; i<lenOfstring; i++ {
-		if a[i] != b[i]{
+	// return early for errors
+	if lenOfstring != lenOfb {
+		return -1, errors.New("the two strings are not of equal length")
+	}
+
+	for i := 0; i < lenOfstring; i++ {
+		if a[i] != b[i] {
 			missMatches = append(missMatches, 1)
 		}
 	}
 	numMissMatches := len(missMatches)
 
-	if lenOfstring != lenOfb {
-			return error("cool")
-		} else if numMissMatches != 0{
-			return numMissMatches, nil
-		} else {
-			return 0, nil
-		}
-}
+	if numMissMatches != 0 {
+		return numMissMatches, nil
+	}
+	return 0, nil
 
-func main()  {
-	z:=Distance("a", "b")
 }
