df9b9b0892e5e167b565ba2c9733e2806d2055ea
komu <komuw05@gmail.com>
2017-09-11 00:53:48 +0300

    dont convert string to byte

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -7,29 +7,20 @@ import (
 const testVersion = 6
 
 // Distance calculates the hamming distance between two strings
-func Distance(x, y string) (int, error) {
-	a := []byte(x)
-	b := []byte(y)
-
+func Distance(a, b string) (int, error) {
 	lenOfstring := len(a)
 	lenOfb := len(b)
-	missMatches := make([]int, 0)
+	missMatches := 0
 
 	// return early for errors
 	if lenOfstring != lenOfb {
 		return -1, errors.New("the two strings are not of equal length")
 	}
-
 	for i := 0; i < lenOfstring; i++ {
 		if a[i] != b[i] {
-			missMatches = append(missMatches, 1)
+			missMatches = missMatches + 1
 		}
 	}
-	numMissMatches := len(missMatches)
-
-	if numMissMatches != 0 {
-		return numMissMatches, nil
-	}
-	return 0, nil
+	return missMatches, nil
 
 }
