384c55a369d8da2ca72473cca804504f7238cd76
komu <komuw05@gmail.com>
2017-09-20 15:53:06 +0300

    finish acronym

--- acronym/acronym.go
+++ acronym/acronym.go
@@ -1,5 +1,35 @@
 package acronym
 
+import (
+	"strings"
+)
+
 const testVersion = 3
 
-func Abbreviate(string) string
+// Abbreviate return acronym of string
+func Abbreviate(input string) string {
+	var res []string
+	var finalRes []string
+
+	f := func(c rune) bool {
+		if string(c) == "-" {
+			return true
+		} else if string(c) == " " {
+			return true
+		}
+		return false
+	}
+	z := strings.FieldsFunc(input, f)
+
+	for _, v := range z {
+		res = append(res, strings.Title(v))
+	}
+	for _, v := range res {
+		finalRes = append(finalRes, string(v[0]))
+	}
+
+	cool := strings.Join(finalRes, "")
+
+	return cool
+
+}
