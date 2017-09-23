649b111313a17df6a7cc062dcd718ae910ac8d34
komu <komuw05@gmail.com>
2017-09-23 19:34:03 +0300

    finish off pangram

--- pangram/pangram.go
+++ pangram/pangram.go
@@ -1,5 +1,22 @@
 package pangram
 
+import (
+	"strings"
+)
+
 const testVersion = 2
 
-func IsPangram(string) bool
+// IsPangram returns true if a given string is a pangram
+func IsPangram(input string) bool {
+	alphabet := []rune{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}
+	lowerInput := strings.ToLower(input)
+	if len(lowerInput) < 26 {
+		return false
+	}
+	for _, v := range alphabet {
+		if !strings.ContainsRune(lowerInput, v) {
+			return false
+		}
+	}
+	return true
+}
